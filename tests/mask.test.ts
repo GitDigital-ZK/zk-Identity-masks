e8249fb62d541aa86e99482773291c5088ee98ea
Richard Kindler <223166946+RickCreator87@users.noreply.github.com>
2026-03-19 10:00:08 -0600

    Add unit tests for Mask functionality
    Signed-off-by: Richard Kindler <223166946+RickCreator87@users.noreply.github.com>

---
+++ tests/mask.test.ts
@@ -0,0 +1,37 @@
+=== FILE: tests/mask.test.ts ===
+import { createMask, getMaskCommitment, Mask } from '../sdk/mask';
+import { poseidonHash } from '../sdk/crypto'; // not listed but we'll assume
+
+describe('Mask', () => {
+it('should create mask with given attributes', () => {
+const attr1 = new Uint8Array(32);
+attr1[0] = 25;
+const attr2 = new Uint8Array(32);
+attr2[0] = 840;
+const mask = createMask([attr1, attr2]);
+expect(mask.getCommitment()).toBeDefined();
+expect(mask.version).toBe(1);
+});
+
+it('should compute commitment consistently', () => {
+const attr = new Uint8Array(32);
+attr[0] = 30;
+const salt = new Uint8Array(32);
+salt[0] = 123;
+const commitment1 = getMaskCommitment([attr], salt);
+const commitment2 = getMaskCommitment([attr], salt);
+expect(commitment1).toEqual(commitment2);
+});
+
+it('should produce different commitments for different salts', () => {
+const attr = new Uint8Array(32);
+attr[0] = 30;
+const salt1 = new Uint8Array(32);
+salt1[0] = 1;
+const salt2 = new Uint8Array(32);
+salt2[0] = 2;
+const commitment1 = getMaskCommitment([attr], salt1);
+const commitment2 = getMaskCommitment([attr], salt2);
+expect(commitment1).not.toEqual(commitment2);
+});
+});
